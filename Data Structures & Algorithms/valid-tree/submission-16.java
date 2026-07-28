9cd09fdb80dc90a404a16388d9cb03fba5401f50
al-ek-sa <alexandra.lishik@gmail.com>
2026-07-28 16:52:49 +0300

    Add: valid-tree - submission-16

---
+++ Data Structures & Algorithms/valid-tree/submission-16.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        int l = edges.length;
+        if(l == 0){
+            return true;
+        }
+        Set<Integer> list = new HashSet<>();
+        list.add(0);
+        Arrays.sort(edges, Comparator.comparingInt(row -> row[0]));
+        for(int i = 0; i < l; i++){
+            int a = edges[i][0];
+            int b = edges[i][1];
+            if(a == b){
+                return false;
+            }
+            if (list.contains(a) && list.contains(b) || !list.contains(a) && !list.contains(b)){
+                return false;
+            } 
+            list.add(a);
+            list.add(b);
+        }
+        return true;
+    }
+}

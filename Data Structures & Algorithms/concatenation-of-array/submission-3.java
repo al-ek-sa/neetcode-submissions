6419c8379ca5af5c21628a7b729214ff3f2a5b47
al-ek-sa <alexandra.lishik@gmail.com>
2026-07-30 17:24:48 +0300

    Add: concatenation-of-array - submission-3

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-3.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[nums.length * 2];
+        int current = 0;
+        for(int i = 0; i < 2; i++){
+            for(int num : nums){
+                ans[current++] = num;
+            }
+        }
+        return ans;
+    }
+}
